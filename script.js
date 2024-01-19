c8979b6c21fbb48a0e38a45b9b49f374b3d3917d
Rahul Prajapati <rp520972@gmail.com>
2024-01-19 23:38:15 +0530

    Portfolio site(1.0)

---
+++ script.js
@@ -0,0 +1,42 @@
+var tablinks = document.getElementsByClassName("tab-links")
+var tabcontents = document.getElementsByClassName("tab-contents")
+function opentab(tabname) {
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+// ---------------------------for resposive------------------
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right="0";
+
+}
+
+function closemenu() {
+    sidemenu.style.right= "-250px"}
+
+// ---------------------------------contact form----------------------
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyKgbyzAqNY45HFKv5qgFR_HPMgKC4kbYW6cT8c8guFQJUt4_Hq3Jvwc922Gh1ffhsY/exec';
+  const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message Sent Successfully"
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+    
